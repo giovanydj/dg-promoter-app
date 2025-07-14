d41e22aac73d2b70a15040c16c64479cf4ea4404
goldchecker <iranalvestorquato@gmail.com>
2025-07-14 00:21:14 -0300

    Update bundle ID to use raianegoisbrandao domain

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.giovanydj.dgpromoter',
+  appId: 'com.raianegoisbrandao.dgpromoter',
   appName: 'DG Promoter',
   webDir: 'www'
 };
